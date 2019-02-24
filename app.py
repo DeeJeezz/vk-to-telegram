10e1133e8baa76b02e21dfb044a4d3938308bf4e
GrobIvanovich <killmaster00431@gmail.com>
2019-02-24 21:35:57 +0500

    Removed cryptography class as unnecessary.

--- app.py
+++ app.py
@@ -3,25 +3,24 @@
 from vk_api import vk_api
 from getpass import getpass
 from print_msg import print_msg
-from crypto import Crypt
 import os.path
 
 # Сделать сохранение данных учетной записи на пк где-то в шифрованном хранилище.
 
 
-def auth():
+def auth() -> list:
     """User enters his login information."""
     username = input('Enter the email or mobile phone number: ')
     passwd = getpass('Enter the password: ')
     return username, passwd
 
 
-def login():
+def login() -> vk_api.VkApi:
     """Sign in to vk.com with login and password."""
     login, password = auth()
     vk_session = vk_api.VkApi(login, password)
     try:
-        vk_session.auth()
+        vk_session.auth(token_only=True)
         print_msg('ok', 'Connected succesfully.')
     except vk_api.AuthError as err:
         print_msg('error', f'{err}!')
@@ -30,17 +29,12 @@ def login():
     return vk_session
 
 
-def check_credentials():
-    """Check existing of config file with stored login data.
-    If exists - decrypt it to login."""
-    cr = Crypt()
-    if os.path.isfile(cr.CONFIG_FILE):
-        pass
-
-
 def main():
     vk_session = login()
     api = vk_session.get_api()
+    response = api.wall.get(count=1)
+    if response['items']:
+        print(response['items'][0])
     # tools = vk_api.VkTools(vk_session)
     # print(api.wall.post(message='Hello world!'))
 
